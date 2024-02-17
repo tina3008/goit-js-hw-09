403cb4f76d7c8be8541b656988d837f170f47646
tina3008 <kicja_3@ukr.net>
2024-02-17 21:52:34 +0200

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,35 +5,30 @@ const saveinfo = { email: '', message: '' };
 
 const parsedinfo = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-
-
-if (parsedinfo == null) {
-  formSet.addEventListener('input', event => {
-    event.preventDefault();
-   const email = event.currentTarget.elements.email.value;
-   const message = event.currentTarget.elements.message.value;
-
-    saveinfo.email = email.trim();
-    saveinfo.message = message.trim();
-    localStorage.setItem('feedback-form-state', JSON.stringify(saveinfo));
-  });
-} else {
+if (parsedinfo !== null) {
   areainfo.value = parsedinfo.message;
   inputinfo.value = parsedinfo.email;
-  
 }
+
+formSet.addEventListener('input', event => {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+
+  saveinfo.email = email.trim();
+  saveinfo.message = message.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(saveinfo));
+});
 
 formSet.addEventListener('submit', evt => {
   evt.preventDefault();
-  
+
   if (areainfo.value.length == 0 || inputinfo.value.length == 0) {
-    console.log(`dsaf` );
+    console.log(`please fill all field`);
   } else {
     console.log(saveinfo);
     localStorage.removeItem('feedback-form-state');
     formSet.reset();
     saveinfo.length = 0;
   }
- 
-
 });
