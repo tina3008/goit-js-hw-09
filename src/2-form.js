1fbe58f5144ce4d5e18668aeaba1161373a6bf8b
tina3008 <kicja_3@ukr.net>
2024-02-17 14:12:11 +0200

    Update 2-form.js

--- src/2-form.js
+++ src/2-form.js
@@ -3,33 +3,37 @@ const inputinfo = document.querySelector('input');
 const areainfo = document.querySelector('textarea');
 const saveinfo = { email: '', message: '' };
 
-const infoStorage = localStorage.getItem('feedback-form-state');
-const parsedinfo = JSON.parse(infoStorage);
+const parsedinfo = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-console.log(`infoStorage=${infoStorage}`);
-console.log(`parsedinfo=${parsedinfo}`);
 
-if (saveinfo.email === '' || saveinfo.message === '') {
+
+if (parsedinfo == null) {
   formSet.addEventListener('input', event => {
     event.preventDefault();
-    const email = event.currentTarget.elements.email.value;
-    const message = event.currentTarget.elements.message.value;
+   const email = event.currentTarget.elements.email.value;
+   const message = event.currentTarget.elements.message.value;
 
     saveinfo.email = email.trim();
     saveinfo.message = message.trim();
     localStorage.setItem('feedback-form-state', JSON.stringify(saveinfo));
-    console.log(saveinfo);
   });
 } else {
-  areainfo.value = saveinfo.message;
-  inputinfo.value = saveinfo.email;
-  console.log(
-    `areainfo.value=${areainfo.value}, inputinfo.value=${inputinfo.value}`
-  );
+  areainfo.value = parsedinfo.message;
+  inputinfo.value = parsedinfo.email;
+  
 }
 
 formSet.addEventListener('submit', evt => {
   evt.preventDefault();
-  localStorage.removeItem('feedback-form-state');
-  formSet.reset();
+  
+  if (areainfo.value.length == 0 || inputinfo.value.length == 0) {
+    console.log(`dsaf` );
+  } else {
+    console.log(saveinfo);
+    localStorage.removeItem('feedback-form-state');
+    formSet.reset();
+    saveinfo.length = 0;
+  }
+ 
+
 });
