1877a16cd973524bf329613a23d3c6b3b8526fe7
tina3008 <kicja_3@ukr.net>
2024-02-18 14:24:44 +0200

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,17 +1,18 @@
 const formSet = document.querySelector('.feedback-form');
-const inputinfo = document.querySelector('input');
-const areainfo = document.querySelector('textarea');
-const saveinfo = { email: '', message: '' };
+const inputinfo = formSet.elements.email;
+const areainfo = formSet.elements.message;
+let saveinfo = { email: '', message: '' };
 
 const parsedinfo = JSON.parse(localStorage.getItem('feedback-form-state'));
 
 if (parsedinfo !== null) {
   areainfo.value = parsedinfo.message;
   inputinfo.value = parsedinfo.email;
+
+  saveinfo = parsedinfo;
 }
 
 formSet.addEventListener('input', event => {
-  event.preventDefault();
   const email = event.currentTarget.elements.email.value;
   const message = event.currentTarget.elements.message.value;
 
@@ -23,12 +24,13 @@ formSet.addEventListener('input', event => {
 formSet.addEventListener('submit', evt => {
   evt.preventDefault();
 
-  if (areainfo.value.length == 0 || inputinfo.value.length == 0) {
+  if (saveinfo.email.length == 0 || saveinfo.message.length == 0) {
     console.log(`please fill all field`);
   } else {
     console.log(saveinfo);
     localStorage.removeItem('feedback-form-state');
     formSet.reset();
-    saveinfo.length = 0;
+    saveinfo.email = '';
+    saveinfo.message = '';
   }
 });
